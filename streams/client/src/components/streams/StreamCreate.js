7ff1bbee1b2131e88e426c3107eb3b0b5b880b3c
Drew <drewharper29@gmail.com>
2018-12-29 13:00:44 -0500

    installed redux-form and began wiring up form components

--- streams/client/src/components/streams/StreamCreate.js
+++ streams/client/src/components/streams/StreamCreate.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
+class StreamCreate extends React.Component{
+    renderInput(formProps){
+        return <input />;
+    }
 
-const StreamCreate = () => {
-    return <div>StreamCreate</div>;
-};
+render() {
 
-export default StreamCreate;
+   return (
+       <form>
+       <Field name="title" component={this.renderInput} />
+        <Field name="description" component={this.renderInput} />
+       </form>
+);
+}
+}
+
+export default reduxForm({
+    form: 'streamCreate'
+})(StreamCreate);
